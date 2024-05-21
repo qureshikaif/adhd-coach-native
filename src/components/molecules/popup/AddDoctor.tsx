8eca141b72a9c582ef356420794ab7258ab8fdbf
qureshikaif <qureshik488@gmail.com>
2024-05-21 21:56:44 +0500

    feat: updated add doctor modal

--- src/components/molecules/popup/AddDoctor.tsx
+++ src/components/molecules/popup/AddDoctor.tsx
@@ -11,9 +11,20 @@ import {
   ButtonText,
   Center,
   Heading,
+  Image,
+  VStack,
+  HStack,
+  Input,
 } from '@gluestack-ui/themed';
 import React from 'react';
 import {ModalProps} from '../../../types/ModalProps';
+import TextSemibold from '../../atoms/Text/TextSemibold';
+import TextBold from '../../atoms/Text/TextBold';
+import TextRegular from '../../atoms/Text/TextRegular';
+import {InputField} from '@gluestack-ui/themed';
+
+const AddDoctorIcon = require('../../../assets/images/add-doctor.png');
+const CloseIcon = require('../../../assets/images/icons/close-white.png');
 
 const AddDoctor = ({showModal, setShowModal, ref}: ModalProps) => {
   return (
@@ -25,38 +36,63 @@ const AddDoctor = ({showModal, setShowModal, ref}: ModalProps) => {
         }}
         finalFocusRef={ref}>
         <ModalBackdrop />
-        <ModalContent>
-          <ModalHeader>
-            <Heading size="lg">Engage with Modals</Heading>
-            <ModalCloseButton>{/* <Icon as={CloseIcon} /> */}</ModalCloseButton>
+        <ModalContent bgColor="#A3BACD">
+          <ModalHeader borderBottomWidth={1} borderBottomColor="#DDDDDD">
+            <TextSemibold
+              text="Add Doctor"
+              color="white"
+              fontSize="$2xl"
+              w="$full"
+              textAlign="center"
+            />
           </ModalHeader>
           <ModalBody>
-            <Text>
-              Elevate user interactions with our versatile modals. Seamlessly
-              integrate notifications, forms, and media displays. Make an impact
-              effortlessly.
-            </Text>
+            <Center>
+              <Image
+                source={AddDoctorIcon}
+                alt="Add Doctor Illustration"
+                w={'$3/5'}
+                h={'$40'}
+                marginVertical={'$4'}
+                resizeMode="cover"
+              />
+              <VStack width={'$full'}>
+                <TextBold text="Doctor ID" fontSize={'$xl'} color="white" />
+                <Input width={'$full'} bgColor="#D7E6ED">
+                  <InputField
+                    display="flex"
+                    alignContent="center"
+                    type="text"
+                    fontFamily="Poppins-Regular"
+                    placeholder={'#29019254'}
+                    fontSize={'$xs'}
+                    placeholderTextColor={'black'}
+                  />
+                </Input>
+              </VStack>
+            </Center>
           </ModalBody>
           <ModalFooter>
-            <Button
-              variant="outline"
-              size="sm"
-              action="secondary"
-              mr="$3"
-              onPress={() => {
-                setShowModal(false);
-              }}>
-              <ButtonText>Cancel</ButtonText>
-            </Button>
-            <Button
-              size="sm"
-              action="positive"
-              borderWidth="$0"
-              onPress={() => {
-                setShowModal(false);
-              }}>
-              <ButtonText>Explore</ButtonText>
-            </Button>
+            <HStack space="sm">
+              <Button
+                flex={1}
+                bgColor="#666666"
+                rounded={'$lg'}
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                <ButtonText>Cancel</ButtonText>
+              </Button>
+              <Button
+                flex={1}
+                bgColor="#648DA0"
+                rounded={'$lg'}
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                <TextRegular text="Confirm" color="white" />
+              </Button>
+            </HStack>
           </ModalFooter>
         </ModalContent>
       </Modal>
