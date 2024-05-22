d2e4b2f2792c1745d9945f782f38ee803cc663ee
qureshikaif <qureshik488@gmail.com>
2024-05-22 12:51:48 +0500

    feat: updated account management screens

--- src/screens/Admin/AccountManagementParents.tsx
+++ src/screens/Admin/AccountManagementParents.tsx
@@ -1,19 +1,21 @@
-import {View, ImageBackground, ScrollView, Box} from '@gluestack-ui/themed';
+import {
+  View,
+  ImageBackground,
+  ScrollView,
+  Box,
+  VStack,
+} from '@gluestack-ui/themed';
 import React from 'react';
 import StatusBarAdmin from '../../components/molecules/StatusBarAdmin';
 import TextBold from '../../components/atoms/Text/TextBold';
 import TextSemibold from '../../components/atoms/Text/TextSemibold';
 import AddNewButton from '../../components/atoms/Buttons/AddNewButton';
 import AddDoctor from '../../components/molecules/popup/AddDoctor';
+import ViewOrRemove from '../../components/atoms/Buttons/ViewOrRemove';
 
 const BackgroundImage = require('../../assets/images/admin-bg-main.png');
 
-// type NavigationType = {
-//   ForgotPasswordVerify: undefined;
-// };
-
 const AccountManagementParents = () => {
-  //   const navigation = useNavigation<NavigationProp<NavigationType>>();
   return (
     <View height={'$full'}>
       <ImageBackground source={BackgroundImage} minHeight={'$full'}>
@@ -22,7 +24,13 @@ const AccountManagementParents = () => {
         <ScrollView paddingHorizontal={'$4'}>
           <TextBold text="Dashboard" fontSize={'$2xl'} />
           <TextSemibold text="App Analytics" fontSize={'$xl'} />
-          <AddNewButton text="Add a new doctor" ModalComponent={AddDoctor} />
+          <VStack space="sm">
+            <AddNewButton text="Add a new doctor" ModalComponent={AddDoctor} />
+            <ViewOrRemove />
+            <ViewOrRemove />
+            <ViewOrRemove />
+            <ViewOrRemove />
+          </VStack>
           {/* <AddNewButton text="Add a new teacher" ModalComponent={AddTeacher} /> */}
         </ScrollView>
       </ImageBackground>
--- src/screens/Admin/AccountManagementTeachers.tsx
+++ src/screens/Admin/AccountManagementTeachers.tsx
@@ -1,10 +1,17 @@
-import {View, ImageBackground, ScrollView, Box} from '@gluestack-ui/themed';
+import {
+  View,
+  ImageBackground,
+  ScrollView,
+  Box,
+  VStack,
+} from '@gluestack-ui/themed';
 import React from 'react';
 import StatusBarAdmin from '../../components/molecules/StatusBarAdmin';
 import TextBold from '../../components/atoms/Text/TextBold';
 import TextSemibold from '../../components/atoms/Text/TextSemibold';
 import AddNewButton from '../../components/atoms/Buttons/AddNewButton';
 import AddDoctor from '../../components/molecules/popup/AddDoctor';
+import ViewOrRemove from '../../components/atoms/Buttons/ViewOrRemove';
 
 const BackgroundImage = require('../../assets/images/admin-bg-main.png');
 
@@ -22,7 +29,13 @@ const AccountManagementTeachers = () => {
         <ScrollView paddingHorizontal={'$4'}>
           <TextBold text="Dashboard" fontSize={'$2xl'} />
           <TextSemibold text="App Analytics" fontSize={'$xl'} />
-          <AddNewButton text="Add a new doctor" ModalComponent={AddDoctor} />
+          <VStack space="sm">
+            <AddNewButton text="Add a new doctor" ModalComponent={AddDoctor} />
+            <ViewOrRemove />
+            <ViewOrRemove />
+            <ViewOrRemove />
+            <ViewOrRemove />
+          </VStack>
           {/* <AddNewButton text="Add a new teacher" ModalComponent={AddTeacher} /> */}
         </ScrollView>
       </ImageBackground>
