aab011c781a67cdb27c479eda99b6c3ebad35552
qureshikaif <qureshik488@gmail.com>
2024-05-23 19:25:14 +0500

    feat: updated recent feedback component

--- src/components/molecules/RecentFeedbacks.tsx
+++ src/components/molecules/RecentFeedbacks.tsx
@@ -1,7 +1,10 @@
-import {View, VStack} from '@gluestack-ui/themed';
+import {Center, HStack, Image, View, VStack} from '@gluestack-ui/themed';
 import React from 'react';
 import TextSemibold from '../atoms/Text/TextSemibold';
 import TextRegular from '../atoms/Text/TextRegular';
+
+const StarFilled = require('../../assets/images/icons/star.png');
+// const StarHollow = require('../../assets/images/icons/star-dark.png');
 
 const RecentFeedbacks = () => {
   return (
@@ -15,6 +18,17 @@ const RecentFeedbacks = () => {
         hardShadow="4"
         p="$4">
         <TextRegular text="Kaif Qureshi" fontSize={'$xl'} />
+        <Center>
+          <HStack justifyContent="center" alignItems="center" h="$4/6">
+            <Image source={StarFilled} w={'$9'} h={'$9'} alt="Star Icon" />
+            <Image source={StarFilled} w={'$9'} h={'$9'} alt="Star Icon" />
+            <Image source={StarFilled} w={'$9'} h={'$9'} alt="Star Icon" />
+            <Image source={StarFilled} w={'$9'} h={'$9'} alt="Star Icon" />
+            <Image source={StarFilled} w={'$9'} h={'$9'} alt="Star Icon" />
+
+            {/* <Image source={StarHollow} w={'$6'} h={'$6'} alt="Star Icon" /> */}
+          </HStack>
+        </Center>
       </VStack>
     </View>
   );
