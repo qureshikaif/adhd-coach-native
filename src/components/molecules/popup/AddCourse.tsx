b8981da4a668fce285f3439ce27bb68ae608408d
qureshikaif <qureshik488@gmail.com>
2024-06-02 01:26:07 +0500

    feat: working on add course functionality

--- src/components/molecules/popup/AddCourse.tsx
+++ src/components/molecules/popup/AddCourse.tsx
@@ -11,6 +11,15 @@ import {
   VStack,
   HStack,
   Input,
+  SelectInput,
+} from '@gluestack-ui/themed';
+import {
+  Select,
+  SelectTrigger,
+  SelectPortal,
+  SelectBackdrop,
+  SelectContent,
+  SelectItem,
 } from '@gluestack-ui/themed';
 import React from 'react';
 import {ModalProps} from '../../../types/ModalProps';
@@ -18,6 +27,9 @@ import TextSemibold from '../../atoms/Text/TextSemibold';
 import TextBold from '../../atoms/Text/TextBold';
 import TextRegular from '../../atoms/Text/TextRegular';
 import {InputField} from '@gluestack-ui/themed';
+import {useQuery} from '@tanstack/react-query';
+import axios from 'axios';
+import {Controller, useForm} from 'react-hook-form';
 
 const AddCourseIcon = require('../../../assets/images/add-course.png');
 
@@ -30,13 +42,25 @@ const fields = [
     name: 'Description',
     placeholder: 'Which topics this course contains',
   },
-  {
-    name: 'Instructor',
-    placeholder: 'Ahmed = 443201',
-  },
+  // {
+  //   name: 'Instructor',
+  //   placeholder: 'Ahmed = 443201',
+  // },
 ];
 
 const AddCourse = ({showModal, setShowModal, ref}: ModalProps) => {
+  const {data: teachers, isLoading} = useQuery({
+    queryKey: ['teachers'],
+    queryFn: async () => {
+      const {data} = await axios.get(
+        'http://192.168.0.107:8080/teacher/get-teachers',
+      );
+      return data;
+      // return data.filter((teacher: any) => teacher.full_name !== null);
+    },
+  });
+  const {control} = useForm();
+  console.log(teachers);
   return (
     <Center>
       <Modal
@@ -88,7 +112,39 @@ const AddCourse = ({showModal, setShowModal, ref}: ModalProps) => {
                     </Input>
                   </VStack>
                 ))}
+                <TextBold text={'Instructor'} fontSize={'$xl'} color="white" />
               </VStack>
+              <Controller
+                control={control}
+                name="teacher"
+                render={({field: {onChange, value}}) => (
+                  <Select onValueChange={onChange}>
+                    <SelectTrigger width={'$full'} bgColor="#D7E6ED">
+                      <SelectInput
+                        display="flex"
+                        alignContent="center"
+                        fontFamily="Poppins-Regular"
+                        fontSize={'$xs'}
+                        placeholderTextColor={'black'}
+                        value={value}
+                      />
+                    </SelectTrigger>
+                    <SelectPortal>
+                      <SelectBackdrop />
+                      <SelectContent bg="whitesmoke">
+                        {teachers &&
+                          teachers.map((teacher: any, index: number) => (
+                            <SelectItem
+                              key={index}
+                              label={teacher.id}
+                              value={teacher.id}
+                            />
+                          ))}
+                      </SelectContent>
+                    </SelectPortal>
+                  </Select>
+                )}
+              />
             </Center>
           </ModalBody>
           <ModalFooter>
