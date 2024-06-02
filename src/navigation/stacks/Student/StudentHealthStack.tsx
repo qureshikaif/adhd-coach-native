64cff414d7769fca1a879a453322204ecd2a9989
qureshikaif <qureshik488@gmail.com>
2024-06-03 01:34:32 +0500

    feat: added student health screen

--- src/navigation/stacks/Student/StudentHealthStack.tsx
+++ src/navigation/stacks/Student/StudentHealthStack.tsx
@@ -10,7 +10,7 @@ const StudentHealthStack = () => {
       initialRouteName="StudentHealth"
       screenOptions={{
         headerShown: false,
-        statusBarColor: '#A3BACD',
+        statusBarColor: '#FFA360',
         statusBarStyle: 'light',
       }}>
       <Stack.Screen name="StudentHealth" component={StudentHealth} />
