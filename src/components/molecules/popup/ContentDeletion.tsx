3aa900135e732103a9a6c8c47caa87be93917487
qureshikaif <qureshik488@gmail.com>
2024-06-02 11:37:36 +0500

    feat: updated admin module

---
+++ src/components/molecules/popup/ContentDeletion.tsx
@@ -0,0 +1,96 @@
+import {
+  ModalFooter,
+  Modal,
+  ModalBackdrop,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Button,
+  Center,
+  Image,
+  HStack,
+} from '@gluestack-ui/themed';
+import React from 'react';
+import {ModalProps} from '../../../types/ModalProps';
+import TextSemibold from '../../atoms/Text/TextSemibold';
+import TextRegular from '../../atoms/Text/TextRegular';
+import {User} from '../../../types/User';
+
+const WarningIcon = require('../../../assets/images/icons/warning.png');
+
+interface AccountDeletionProps extends ModalProps {
+  user?: User;
+}
+
+const ContentDeletion = ({
+  showModal,
+  setShowModal,
+  ref,
+}: AccountDeletionProps) => {
+  return (
+    <Center>
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        size="lg"
+        finalFocusRef={ref}>
+        <ModalBackdrop />
+        <ModalContent bgColor="#A3BACD" rounded={'$2xl'}>
+          <ModalHeader borderBottomWidth={1} borderBottomColor="#DDDDDD">
+            <TextSemibold
+              text="Content Deletion"
+              color="white"
+              fontSize="$2xl"
+              w="$full"
+              textAlign="center"
+            />
+          </ModalHeader>
+          <ModalBody>
+            <Center>
+              <Image
+                source={WarningIcon}
+                alt="Add Doctor Illustration"
+                w={'$1/3'}
+                h={'$24'}
+                marginVertical={'$4'}
+                resizeMode="contain"
+              />
+              <TextSemibold
+                text="Are you sure you want to delete this content?"
+                color="white"
+                fontSize="$xl"
+                textAlign="center"
+              />
+            </Center>
+          </ModalBody>
+          <ModalFooter>
+            <HStack space="sm">
+              <Button
+                flex={1}
+                bgColor="#666666"
+                rounded={'$lg'}
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                <TextRegular text="Cancel" color="white" />
+              </Button>
+              <Button
+                flex={1}
+                bgColor="#648DA0"
+                rounded={'$lg'}
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                <TextRegular text="Confirm" color="white" />
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Center>
+  );
+};
+
+export default ContentDeletion;
--- src/screens/Admin/CourseManagement/ContentManagementCourse.tsx
+++ src/screens/Admin/CourseManagement/ContentManagementCourse.tsx
@@ -5,19 +5,37 @@ import TextSemibold from '../../../components/atoms/Text/TextSemibold';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
 import AddCourse from '../../../components/molecules/popup/AddCourse';
 import Modify from '../../../components/atoms/Buttons/Modify';
+import {useQuery} from '@tanstack/react-query';
+import axios from 'axios';
+import Loading from '../../Loading';
+import {Course} from '../../../types/Course';
 
 const BackgroundImage = require('../../../assets/images/admin-content-bg.png');
 
 const ContentManagementCourse = () => {
   const height = useBottomTabBarHeight();
+
+  const {data: courses, isLoading} = useQuery({
+    queryKey: ['courses'],
+    queryFn: async () => {
+      const {data} = await axios.get(
+        'http://192.168.0.107:8080/admin/get-courses',
+      );
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading bgImage={BackgroundImage} />;
+  }
   return (
     <ImageBackground source={BackgroundImage} minHeight={'$full'}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <VStack space="xs">
           <AddNewButton text="Add a new course" ModalComponent={AddCourse} />
           <TextSemibold text="Modify" fontSize={'$xl'} />
-          {Array.from({length: 6}).map((_, index) => (
-            <Modify key={index} />
+          {courses.map((course: Course, index: number) => (
+            <Modify key={index} content={course} />
           ))}
         </VStack>
         <Box height={height * 2.5} />
