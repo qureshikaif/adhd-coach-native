b9048b9e3800e87c182a63eb446b8dca42f24b89
qureshikaif <qureshik488@gmail.com>
2024-06-02 14:26:02 +0500

    chore: updated side buttons

--- src/screens/Parent/ParentArticleList.tsx
+++ src/screens/Parent/ParentArticleList.tsx
@@ -7,7 +7,7 @@ import {
 } from '@gluestack-ui/themed';
 import React from 'react';
 import TextBold from '../../components/atoms/Text/TextBold';
-import SideButton from '../../components/atoms/Buttons/VORDoctor';
+import SideButton from '../../components/atoms/Buttons/SideButton';
 import StatusBarTeacher from '../../components/molecules/StatusBarTeacher';
 
 const BackgroundImage = require('../../assets/images/parent-main-bg.png');
--- src/screens/Parent/ParentPersonaProfile.tsx
+++ src/screens/Parent/ParentPersonaProfile.tsx
@@ -6,37 +6,27 @@ import {
   Box,
   Center,
   Image,
-  HStack,
-  Input,
-  InputField,
   Button,
 } from '@gluestack-ui/themed';
 import React from 'react';
-import StatusBarAdmin from '../../components/molecules/StatusBarAdmin';
 import TextBold from '../../components/atoms/Text/TextBold';
-import TextSemibold from '../../components/atoms/Text/TextSemibold';
-import AddNewButton from '../../components/atoms/Buttons/AddNewButton';
-import {VStack} from '@gluestack-ui/themed';
-import BackButton from '../../components/atoms/Buttons/BackButton';
 import StatusBarTeacher from '../../components/molecules/StatusBarTeacher';
 import SideScreenButton from '../../components/atoms/Buttons/SideScreenButton';
+import TextSemibold from '../../components/atoms/Text/TextSemibold';
+import {useStore} from '../../store';
+import {handleLogout} from '../../helpers/handleLogout';
 const TeacherPic = require('../../assets/images/icons/ProfilePicSana.png');
 const BackgroundImage = require('../../assets/images/TeacherProfileSetting.png');
-const Avatar = require('../../assets/images/avatars/login.png');
 
 const ParentPersonalProfile = () => {
+  const store = useStore();
   //   const navigation = useNavigation<NavigationProp<NavigationType>>();
   return (
     <View height={'$full'}>
       <ImageBackground source={BackgroundImage} minHeight={'$full'}>
+        <StatusBarTeacher text="Profile" />
+
         <ScrollView paddingHorizontal={'$4'}>
-          <HStack justifyContent="center" alignItems="center">
-            <TextSemibold
-              text="Profile "
-              paddingRight={'$8'}
-              fontSize={'$3xl'}
-            />
-          </HStack>
           <Box height={'$10'} />
 
           <Box flex={1}>
@@ -59,27 +49,23 @@ const ParentPersonalProfile = () => {
                   <Image
                     source={TeacherPic}
                     alt="Teacher pic"
-                    style={{width: '100%', height: '100%'}}
+                    w="$full"
+                    h="$full"
                   />
                 </Box>
-                <Text mt={4} fontFamily="Poppins-Regular">
-                  Sana Zehra
-                </Text>
+                <TextSemibold mt={4} text="Sana Zehra" />
               </Center>
               <Box height={'$10'} />
               <SideScreenButton text="Profile info" />
-
-              <Box height={'$5'} />
-              <SideScreenButton text="child Info" />
               <Box height={'$5'} />
               <SideScreenButton text="Profile Setting" />
-              <Box height={'$16'} />
-
               <Box flex={1} justifyContent="center" alignItems="center">
                 <Button
+                  onPress={() => handleLogout(store)}
                   android_ripple={{color: '#DEB5B5'}}
                   hardShadow="3"
-                  width={120}
+                  width={'$48'}
+                  h="$12"
                   borderColor="black"
                   bg={'#EDECD7'}
                   borderWidth={1}
