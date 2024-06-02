823b7252960e23b13518b48a339d60bbcde66be3
aleenaahmed21 <aleenaahmed142002@gmail.com>
2024-06-02 17:39:43 +0500

    notif added in rps screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {GluestackUIProvider, createConfig} from '@gluestack-ui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import MainNavigator from './src/navigation/MainNavigator';
+import RockPaperScissor from './src/screens/StudentModule/RockPaperScissor';
 
 const config = createConfig({
   ...defaultConfig,
@@ -27,8 +28,8 @@ function App(): React.JSX.Element {
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
         <GluestackUIProvider config={config}>
-          <MainNavigator />
-          {/* <RockPaperScissor /> */}
+          {/* <MainNavigator /> */}
+          <RockPaperScissor />
           {/* <RockPaperScissor/> */}
         </GluestackUIProvider>
       </QueryClientProvider>
