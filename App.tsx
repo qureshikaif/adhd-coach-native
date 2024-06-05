81263d444f6ba85b421051b27944f8bd402a6a60
aleenaahmed21 <aleenaahmed142002@gmail.com>
2024-06-05 15:58:36 +0500

    shape screen and doctor screen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import TeacherProfileSetting from './src/screens/Teacher/TeacherProfileSetting';
 import MainNavigator from './src/navigation/MainNavigator';
 import DoctorsList from './src/screens/Parent/DoctorList';
 import ChildProgressList from './src/screens/Parent/ChildProgressList';
+import ShapesEnglish from './src/screens/StudentModule/ShapesEnglish';
 
 const config = createConfig({
   ...defaultConfig,
@@ -32,7 +33,8 @@ function App(): React.JSX.Element {
         <GluestackUIProvider config={config}>
           {/* <MainNavigator /> */}
           {/* <TeacherProfileSetting/> */}
-          <ChildProgressList />
+          {/* <ChildProgressList /> */}
+          <ShapesEnglish/>
         </GluestackUIProvider>
       </QueryClientProvider>
     </NavigationContainer>
--- src/screens/StudentModule/ShapesEnglish.tsx
+++ src/screens/StudentModule/ShapesEnglish.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   View,
   ImageBackground,
@@ -10,13 +11,47 @@ import {
 } from '@gluestack-ui/themed';
 
 import TextSemibold from '../../components/atoms/Text/TextSemibold';
-import React from 'react';
 
 const BackgroundImage = require('../../assets/images/shapes-bg.png');
-const Triangle = require('../../assets/images/triangle-red.png');
-const CircleCartoon = require('../../assets/images/cartoon-circle.png');
+const shapes = [
+  {
+    name: 'Triangle',
+    color: 'RED',
+    image: require('../../assets/images/triangle-red.png'),
+  },
+  {
+    name: 'Circle',
+    color: 'GREY',
+    image: require('../../assets/images/circle-gray.png'),
+  },
+  {
+    name: 'Square',
+    color: 'BLUE',
+    image: require('../../assets/images/square-blue.png'),
+  },
+  {
+    name: 'Rhombus',
+    color: 'YELLOW',
+    image: require('../../assets/images/rhombus-yellow.png'),
+  },
+  {
+    name: 'Hexogon',
+    color: 'GREEN',
+    image: require('../../assets/images/hexagon-green.png'),
+  },
+];
 
 const ShapesEnglish = () => {
+  const [currentShapeIndex, setCurrentShapeIndex] = useState(0);
+
+  const handleNextShape = () => {
+    setCurrentShapeIndex((prevIndex) =>
+      prevIndex === shapes.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const currentShape = shapes[currentShapeIndex];
+
   return (
     <View height={'$full'}>
       <ImageBackground
@@ -31,20 +66,25 @@ const ShapesEnglish = () => {
             padding={'$4'}>
             <View />
           </HStack>
-          {/* <TextRegular fontSize={'$5xl'} text="Welcome" fontStyle="italic" /> */}
           <VStack h={'85%'} justifyContent="center" alignItems="center">
             <Center>
+              <TextSemibold
+                text="Hello! What is my name and what color am I?"
+                fontSize={'$2xl'}
+                padding={'$1'}
+              />
               <Box
                 rounded={'$2xl'}
                 bgColor="#DEB5B5"
                 h={'$40'}
                 w={'$40'}
                 paddingLeft={16}
+                paddingRight={8} // Add paddingRight here
                 paddingTop={10}>
                 <Image
-                  source={Triangle}
-                  alt="Triangle red"
-                  size={'xl'}
+                  source={currentShape.image}
+                  alt={currentShape.name}
+                  size={'full'}
                   resizeMode="contain"
                 />
               </Box>
@@ -57,7 +97,7 @@ const ShapesEnglish = () => {
                     alignItems="center"
                     space="lg">
                     <TextSemibold
-                      text="Shape: Triangle"
+                      text={`Shape: ${currentShape.name}`}
                       fontSize={'$2xl'}
                       padding={'$1'}
                     />
@@ -72,9 +112,15 @@ const ShapesEnglish = () => {
                     alignItems="center"
                     space="lg">
                     <TextSemibold
-                      text="Color: RED"
+                      text="Color:"
                       fontSize={'$2xl'}
-                      color="$red600"
+                      color="$black"
+                      padding={'$1'}
+                    />
+                    <TextSemibold
+                      text={currentShape.color}
+                      fontSize={'$2xl'}
+                      color={`$${currentShape.color.toLowerCase()}`}
                       padding={'$1'}
                     />
                   </HStack>
@@ -83,18 +129,13 @@ const ShapesEnglish = () => {
 
               <Box height={'$10'} />
               <Pressable
+                onPress={handleNextShape}
                 bgColor="#DEB5B5"
                 paddingHorizontal={'$8'}
                 paddingVertical={'$2'}
                 rounded={'$2xl'}>
                 <TextSemibold text="Next" fontSize={'$lg'} />
               </Pressable>
-              <Box height={'$10'} />
-              <VStack width={'$full'} rowGap={'$4'}>
-                <Box h={10}>
-                  <Image source={CircleCartoon} alt="Cartoon circle" />
-                </Box>
-              </VStack>
             </Center>
           </VStack>
         </Center>
